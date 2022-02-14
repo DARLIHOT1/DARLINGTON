9fc6847fb09cf5258eae41ef394e7755c8f2e6b3
DARLIHOT1 <99670069+DARLIHOT1@users.noreply.github.com>
2022-02-14 23:42:24 +0100

    Update darlington.py

--- darlington.py
+++ darlington.py
@@ -53,7 +53,7 @@ def main():
 	print("\033[92;1m  [1] START CRACK FB")
 	print("\033[93;1m  [2] HOW TO GET ACCESS TOKEN")
 	print("\033[94;1m  [3] UPDATE TOOL")
-	print("\033[96;1m  [J] JOIN AND ENJOY
+	print("\033[96;1m  [J] JOIN MR. ERROR GROUP \033[92;1m✘\033[91;1m✘")
 	print("\033[90;1m  [0] EXIT")
 	print("")
 	log_sel()
